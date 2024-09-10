bf601ab9037ab7f80fe07561f7ade458635cd838
jae-le <jwlme@naver.com>
2024-09-10 11:47:48 +0900

    Changes

--- src/Array/Array_6.java
+++ src/Array/Array_6.java
@@ -7,7 +7,7 @@ import java.io.OutputStreamWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class Array_5 {
+public class Array_6 {
     public static void main(String[] args) {
         try {
             // 입력을 위한 BufferedReader와 출력을 위한 BufferedWriter 선언
@@ -18,24 +18,24 @@ public class Array_5 {
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
 
-            int[] basket = new int[N];
-            String[] s2 = new String[3];
 
-            int i,j,k;
-            for (int a = 0; a < M; a++) {
-                s2 = br.readLine().split(" ");
-                i = Integer.parseInt(s2[0]);
-                j = Integer.parseInt(s2[1]);
-                k = Integer.parseInt(s2[2]);
-
-                for (int b = i-1; b <=j-1 ; b++) {
-                    basket[b]=k;
-                }
+            int[] basket = new int[N+1];
+            for (int i = 1; i < N+1; i++) {
+               basket[i] = i;
             }
-            for (int a = 0; a < N; a++) {
-                bw.write(String.valueOf(basket[a])+" ");
-            }
+            int a,b,c;
+            for (int i = 0; i < M; i++) {
+                StringTokenizer st1 = new StringTokenizer(br.readLine());
+                a = Integer.parseInt(st1.nextToken());
+                b = Integer.parseInt(st1.nextToken());
 
+                c =basket[a];
+                basket[a] = basket[b];
+                basket[b] = c;
+            }
+            for (int i = 1; i < N+1; i++) {
+                bw.write(String.valueOf(basket[i])+" ");
+            }
 
             bw.flush();
             br.close();
---
+++ src/Array/Array_7.java
@@ -0,0 +1,39 @@
+package Array;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Array_7 {
+    public static void main(String[] args) {
+        try {
+            // 입력을 위한 BufferedReader와 출력을 위한 BufferedWriter 선언
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            boolean[] arr = new boolean[31];
+
+            for (int i = 1; i < 29; i++) {
+                int N = Integer.parseInt(br.readLine());
+                arr[N]=true;
+            }
+
+            for (int i = 1; i < 31; i++) {
+                if (arr[i] == false) {
+                    bw.write(i + "\n");
+                }
+            }
+
+
+
+            bw.flush();
+            br.close();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
