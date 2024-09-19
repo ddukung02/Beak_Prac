68cf4c397c092b054422b1f48d5604b8388942ba
ddukung02 <yooiwn@gmail.com>
2024-09-19 14:25:02 +0900

    Changes

---
+++ src/Step_String/String_6_2.java
@@ -0,0 +1,39 @@
+package Step_String;
+
+import java.io.*;
+
+public class String_6_2 {
+    public static void main(String[] args) {
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            String S = br.readLine();
+
+            int[] position = new int[26];
+
+            for(int i = 0; i < 26; i++){
+                position[i] = -1;
+            }
+
+            for(int i = 0; i < S.length(); i++){
+                char c = S.charAt(i);
+                int index = c - 'a';
+
+                if(position[index] == -1){
+                    position[index] = i;
+                }
+            }
+
+            for (int i = 0; i < 26; i++) {
+                bw.write(position[i] + " ");
+            }
+
+            bw.flush();
+            bw.close();
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
